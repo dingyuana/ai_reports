bd092f517ec8f053b361214ecc75ed82fe50e23e
BehaviDetect Developer <developer@example.com>
2025-12-30 10:29:04 +0800

    upload zip files

--- front/script.js
+++ front/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'http://127.0.0.1:8000';
+    const API_BASE_URL = window.location.origin;
 
     let selectedDirectory = null;
 
